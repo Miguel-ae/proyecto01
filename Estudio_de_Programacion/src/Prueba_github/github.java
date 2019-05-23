a738dc79caa3f4539e88711d94be0b211862c445
Migui <Migui@192.168.1.220>
2019-05-23 23:25:44 +0200

    Segundo Commit

--- Estudio_de_Programacion/src/Prueba_github/github.java
+++ Estudio_de_Programacion/src/Prueba_github/github.java
@@ -5,7 +5,8 @@ public class github {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola!");
+		System.out.println("Hola");
+		System.out.println("Hola de nuevo!");
 		
 	}
 
