07c44b71f87da9a9526f867cb4a8d76f9be526d9
Migui <Migui@192.168.1.220>
2019-05-26 09:46:29 +0200

    Ultima actualizacion

---
+++ Examen/src/Cadenas/Principal.java
@@ -0,0 +1,14 @@
+package Cadenas;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cadena cadena01 = new Cadena();
+		
+		cadena01.devolverSubcadena("HAMBURGUER", 4, 4);
+		
+	}
+
+}
